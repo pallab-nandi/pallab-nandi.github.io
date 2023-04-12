27af2456fd26aafde1828020cd75f98cca876025
pallab-nandi <pallabnandi@outlook.in>
2023-04-12 15:39:13 +0530

    added: home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://in.linkedin.com/in/pallabnandi" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a href="https://github.com/pallab-nandi" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  )
+}
